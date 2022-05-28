a59fc344323cfcf226c63f6bf74da0ad2b503d33
Brad Bissell <bissell.brad@gmail.com>
2022-05-28 14:41:00 -0400

    working service

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
-var app = express();
+const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res, next) => {
+  const file = req.file;
+  console.log("file: ", file);
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size,
+  });
+});
 
-
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+app.listen((port = process.env.PORT || 3000), () => {
+  console.log("Your app is listening on port " + port);
 });
